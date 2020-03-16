ce87acebe746bb06a9f257970fb6e69828f091e2
rdgnunes <rdgnunes@hotmail.com>
2020-03-16 23:10:52 +0000

    Inicio Calculadora

--- Controllers/CalculadoraController.cs
+++ Controllers/CalculadoraController.cs
@@ -7,12 +7,57 @@ namespace CalculadoraRestAPI.Controllers
     [ApiController]
     public class CalculadoraController : ControllerBase
     {
-        [HttpGet("{firstNumber}/{secondNumber}", Name = "Sum")]
+
+        [HttpGet("Sum/{firstNumber}/{secondNumber}", Name = "Sum")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Input Inválido");
+        }
+
+        [HttpGet("Sub/{firstNumber}/{secondNumber}", Name = "Sub")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Input Inválido");
+        }
+
+        [HttpGet("Mult/{firstNumber}/{secondNumber}", Name = "Mult")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Input Inválido");
+        }
+
+        [HttpGet("Div/{firstNumber}/{secondNumber}", Name = "Div")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Input Inválido");
+        }
+
+        [HttpGet("Med/{firstNumber}/{secondNumber}", Name = "Med")]
+        public IActionResult Med(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
                 return Ok(sum.ToString());
             }
             return BadRequest("Input Inválido");
